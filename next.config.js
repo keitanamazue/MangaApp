00f9cc1dbd344508fe1c3e9ffa1f8463e3311683
keita namazue <keitanamazue@gmail.com>
2022-07-30 22:02:41 +0900

    ➕ version　change

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
-  swcMinify: true,
-  image: ["thumbnail.image.rakuten.co.jp", "cover.open.kobo.com"],
+  images: {
+    domains: ["thumbnail.image.rakuten.co.jp", "cover.openbd.jp"],
+  },
 }
-
-module.exports = nextConfig
